ddbf782dbf3ffb49a769676813fdd3380b3fa60b
Ryan <ryanhilliard23@outlook.com>
2025-09-23 17:58:56 -0400

    Implement routing for sidebar pages to show corresponding info

--- frontend/src/Main.jsx
+++ frontend/src/Main.jsx
@@ -1,6 +1,14 @@
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
+import React from 'react'; 
+import { BrowserRouter } from 'react-router-dom';
 
-createRoot(document.getElementById('root')).render(
-  <App />
-)
+const root = createRoot(document.getElementById('root'));
+
+root.render(
+  <React.StrictMode>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </React.StrictMode>
+);
