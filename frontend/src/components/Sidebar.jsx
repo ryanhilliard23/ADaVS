bc6a5223ba1a99d464809396b647703afead5688
Ryan <ryanhilliard23@outlook.com>
2025-09-28 21:31:58 -0400

    Update dashboard design, navbar, logo, and icons

--- frontend/src/components/Sidebar.jsx
+++ frontend/src/components/Sidebar.jsx
@@ -1,24 +1,33 @@
 import React from 'react';
-import { NavLink } from 'react-router-dom'; 
+import { NavLink } from 'react-router-dom';
+import { MdDashboard, MdStorage, MdSecurity } from 'react-icons/md';
 import '../css/global.css';
+import adavsLogo from '../assets/adavs.png';
 
 const Sidebar = () => {
   return (
     <nav className="sidebar-container">
       <div className="sidebar-header">
+        <img src={adavsLogo} alt="ADaVS Logo" className="header-logo" />
         <h2>ADaVS</h2>
       </div>
-      <ul className="sidebar-nav">
-        <NavLink to="/" className={({ isActive }) => isActive ? "nav-item active" : "nav-item"}>
-          Dashboard
-        </NavLink>
-        <NavLink to="/assets" className={({ isActive }) => isActive ? "nav-item active" : "nav-item"}>
-          Assets
-        </NavLink>
-        <NavLink to="/vulnerabilities" className={({ isActive }) => isActive ? "nav-item active" : "nav-item"}>
-          Vulnerabilities
-        </NavLink>
-      </ul>
+
+      <div className="nav-links-container">
+        <ul className="sidebar-nav">
+          <NavLink to="/" className={({ isActive }) => isActive ? "nav-item active" : "nav-item"}>
+            <MdDashboard className="nav-icon" />
+            Dashboard
+          </NavLink>
+          <NavLink to="/assets" className={({ isActive }) => isActive ? "nav-item active" : "nav-item"}>
+            <MdStorage className="nav-icon" />
+            Assets
+          </NavLink>
+          <NavLink to="/vulnerabilities" className={({ isActive }) => isActive ? "nav-item active" : "nav-item"}>
+            <MdSecurity className="nav-icon" />
+            Vulnerabilities
+          </NavLink>
+        </ul>
+      </div>
     </nav>
   );
 };
