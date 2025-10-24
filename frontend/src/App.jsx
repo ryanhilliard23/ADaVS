458c3e119811483747d629f3b279e15878367e87
Ryan <ryanhilliard23@outlook.com>
2025-10-24 12:47:13 -0400

    Update front end react files for responsiveness

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,28 +1,71 @@
-import { Routes, Route, Navigate } from 'react-router-dom';
+// STEP 1: Import useEffect
+import React, { useEffect } from 'react'; 
+import { Routes, Route, Navigate, useLocation } from 'react-router-dom';
 import ProtectedLayout from './components/ProtectedLayout';
 import RedirectIfLoggedIn from './components/RedirectIfLoggedIn';
 import LandingPage from './pages/LandingPage';
 import Dashboard from './pages/Dashboard';
 import Assets from './pages/Assets';
 import Vulnerabilities from './pages/Vulnerabilities';
+import Particles from './components/Particles';
 import './css/global.css'; 
 
 function App() {
+  const location = useLocation(); 
+  const isLandingPage = location.pathname === '/';
+
+  useEffect(() => {
+    const baseTitle = 'ADaVS';
+    let pageTitle = 'Home';
+
+    switch (location.pathname) {
+      case '/':
+        pageTitle = 'Home';
+        break;
+      case '/dashboard':
+        pageTitle = 'Dashboard';
+        break;
+      case '/assets':
+        pageTitle = 'Assets';
+        break;
+      case '/vulnerabilities':
+        pageTitle = 'Vulnerabilities';
+        break;
+      default:
+        pageTitle = 'Page Not Found';
+    }
+    
+    document.title = `${baseTitle} | ${pageTitle}`;
+
+  }, [location]);
+
   return (
-    <Routes>
-      {/* Redirects if you are already logged in */}
-      <Route element={<RedirectIfLoggedIn />}>
-        <Route path="/" element={<LandingPage />} />
-      </Route>
+    <>
+      <Particles
+        particleColors={['#ffffff', '#ffffff']}
+        particleCount={200} 
+        particleSpread={10}
+        speed={0.05} 
+        particleBaseSize={50}
+        moveParticlesOnHover={isLandingPage}  
+        alphaParticles={true}
+        disableRotation={false}
+      />
 
-      <Route element={<ProtectedLayout />}>
-        <Route path="/dashboard" element={<Dashboard />} />
-        <Route path="/assets" element={<Assets />} />
-        <Route path="/vulnerabilities" element={<Vulnerabilities />} />
-      </Route>
+      <Routes>
+        <Route element={<RedirectIfLoggedIn />}>
+          <Route path="/" element={<LandingPage />} />
+        </Route>
 
-      <Route path="*" element={<Navigate to="/" replace />} />
-    </Routes>
+        <Route element={<ProtectedLayout />}>
+          <Route path="/dashboard" element={<Dashboard />} />
+          <Route path="/assets" element={<Assets />} />
+          <Route path="/vulnerabilities" element={<Vulnerabilities />} />
+        </Route>
+
+        <Route path="*" element={<Navigate to="/" replace />} />
+      </Routes>
+    </>
   );
 }
 
