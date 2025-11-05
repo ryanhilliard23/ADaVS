f39c6c83b3c45be66bc4ca04df38c7d76276e552
Ryan <ryanhilliard23@outlook.com>
2025-11-04 21:06:39 -0500

    Update fetch url for frontend

--- frontend/src/components/Login.jsx
+++ frontend/src/components/Login.jsx
@@ -23,7 +23,7 @@ const Login = ({ onSwitchMode }) => {
     formData.append('password', password);
 
     try {
-      const response = await fetch(`${API_BASE_URL}/users/login`, {
+      const response = await fetch(`https://adavs-backend.onrender.com/api/users/login`, {
         method: 'POST',
         headers: {
           'Content-Type': 'application/x-www-form-urlencoded',
